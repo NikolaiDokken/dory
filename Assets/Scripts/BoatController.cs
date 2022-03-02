006b83fb2476e392d053673981fcb330484df09b
Hanne Loftesnes <Hanne.loftesnes@gmail.com>
2022-03-02 09:04:25 +0100

    Some cleanup of files

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -2,40 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BoatController : MonoBehaviour
 {
+    // Start is called before the first frame update
+    public GameObject propeler;
+    public GameObject balloonBoat;
+    public float turningSpeed;
+    public float drivingSpeed;
+    public float rotationSpeed;
+    public Rigidbody rigidBody;
 
-   
-    public float speed = 1.0f;
-    public float steerSpeed = 1.0f;
-    public float movementThresold = 10.0f;
-
-    float verticalInput;
-    float movementFactor;
     float horizontalInput;
-    float steerFactor;
-  
+    float verticalInput;
+    float angleMax = 3.14f;
+    float angleMin = -3.14f;
+    [SerializeField]
+    float angle = 0.0f;
+    float lx = 0.0f;
+
+
+    void Start()
+    {
+        Debug.Log("Moshimoshi");
+        lx = Mathf.Abs(propeler.transform.position.x - balloonBoat.transform.position.x);        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Movement();
         Steer();
-    }
-
-
-    void Movement()
-    {
-        verticalInput = Input.GetAxis("Vertical");
-        movementFactor = Mathf.Lerp(movementFactor, verticalInput, Time.deltaTime / movementThresold);
-        transform.Translate(movementFactor * speed, 0.0f, 0.0f);
+        Movement();
     }
 
     void Steer()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        steerFactor = Mathf.Lerp(steerFactor, horizontalInput * verticalInput, Time.deltaTime / movementThresold);
-        transform.Rotate(0.0f, steerFactor * steerSpeed, 0.0f);
+        angle += horizontalInput * turningSpeed * Time.deltaTime;
+        angle = Mathf.Clamp(angle,angleMin, angleMax);
     }
+
+    void Movement()
+    {
+        verticalInput = verticalInput = Input.GetAxis("Vertical");
+        float Fx = verticalInput * drivingSpeed * Mathf.Cos(angle);
+        float Fz = -verticalInput * drivingSpeed * Mathf.Sin(angle);
+        rigidBody.AddRelativeForce(new Vector3(Fx, 0.0f, 0.0f), ForceMode.Acceleration);
+
+        rigidBody.AddTorque(new Vector3(0f, - lx * Fz*rotationSpeed, 0.0f));
+
+    }
+
+
 }
