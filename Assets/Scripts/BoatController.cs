1fcf5e52aee9a0508ed657636e81bbc88c8b62d6
Hanne Loftesnes <Hanne.loftesnes@gmail.com>
2022-03-16 10:52:50 +0100

    synchronized wheel rotation with turning. Changed update to FixedUpdate

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class BoatController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject propeler;
     public GameObject balloonBoat;
-    public float turningSpeed;
     public float drivingSpeed;
     public float rotationSpeed;
     public Rigidbody rigidBody;
 
-    float horizontalInput;
     float verticalInput;
-    float angleMax = 1.57f;
-    float angleMin = -1.57f;
     [SerializeField]
     float angle = 0.0f;
     float lx = 0.0f;
@@ -28,22 +23,15 @@ public class BoatController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Steer();
+        angle = - wheelRotation.angle * Mathf.PI/900;
         Movement();
-    }
-
-    void Steer()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        angle += horizontalInput * turningSpeed * Time.deltaTime;
-        angle = Mathf.Clamp(angle, angleMin, angleMax);
     }
 
     void Movement()
     {
-        verticalInput = verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         float Fx = verticalInput * drivingSpeed * Mathf.Cos(angle);
         float Fz = -verticalInput * drivingSpeed * Mathf.Sin(angle);
         rigidBody.AddRelativeForce(new Vector3(Fx, 0.0f, 0.0f), ForceMode.Acceleration);
